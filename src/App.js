5fd0aa49402306815905ab5e2583c683511779d7
huynh do ngoc <ngochuynh1991@gmail.com>
2018-05-29 13:32:22 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
 import React from 'react';
+import { Switch, Link, Route, Redirect } from 'react-router-dom';
+import { HomePage, User } from './Main';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      base64: '',
-      error: '',
-      result: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (!this.state.state) {
-      this.setState({ error: 'Input is empty' });
-    }
-    if (!this.state.error) {
-      const { base64 } = this.state;
-      const buff = Buffer.from(base64, 'base64');
-      const result = `data:application/pdf;base64,${buff.toString('base64')}`;
-
-      this.setState({ result });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {
-          !this.state.result ?
-            <div className="container">
-              <div className="col-md-10 col-md-offset-1">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group">
-                    <h4>Generate Document from base 64 to PDF</h4>
-                    <textarea
-                      className="form-control"
-                      rows="18"
-                      name="base64"
-                      placeholder="Input base64 here"
-                      value={this.state.base64}
-                      onChange={this.handleChange}
-                    />
-                    { this.state.error ? <div className="has-error">Checkbox with error</div> : ''}
-                  </div>
-                  <button type="submit" className="btn btn-default">
-                    Submit
-                  </button>
-                </form>
-              </div>
-            </div> :
-/* eslint-disable */
-						<object data={this.state.result} type="application/pdf"  width="100%" height="100%" />
-/* eslint-disable */
-        }
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="Main">
+    <div className="menu">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/user">User</Link></li>
+      </ul>
+    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/user" component={User} />
+      <Redirect to="/" />
+    </Switch>
+  </div>
+);
 
 export default App;
